cc2a82af1a7a0497b58f642d4821cdb6b4fba5d0
Mariana Vallejos <mariana.vallejos@assuresoft.tech>
2025-09-19 19:51:30 -0400

    feat: flipp animation added in flashcards when it's clicked

--- src/components/flashcards/Flashcard.tsx
+++ src/components/flashcards/Flashcard.tsx
@@ -1,8 +1,9 @@
-import { AiOutlineDelete } from "react-icons/ai";
+import { useState } from "react";
 import { Status, type Flashcard } from "../../types/flashcard";
 import { topicColors } from "../../types/topics";
 import "./Flashcard.css";
 import { MdOutlineEdit } from "react-icons/md";
+import { AiOutlineDelete } from "react-icons/ai";
 
 type Props = {
   flashcard: Flashcard;
@@ -10,58 +11,81 @@ type Props = {
   onDelete?: (id: number) => void;
 };
 
-const FlashcardComponent = (flashcard : Flashcard) => {
+const FlashcardComponent = ( flashcard: Flashcard) => {
+  const [flipped, setFlipped] = useState(false);
+
   return (
-    <article className="relative bg-white flashcard-bg rounded-xl shadow-lg p-6 flex flex-col justify-between hover:shadow-xl transition group">
-      <div className="absolute top-3 right-3 flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
-        <button
-          onClick={() => {}}
-          className="p-2 rounded-full bg-blue-100 text-blue-600 hover:bg-blue-200 transition"
-          title="Editar"
-        >
-          <MdOutlineEdit />
-        </button>
-        <button
-          onClick={() => {}}
-          className="p-2 rounded-full bg-red-100 text-red-600 hover:bg-red-200 transition"
-          title="Eliminar"
-        >
-          <AiOutlineDelete />
-        </button>
-      </div>
+    <article
+      className="relative h-60  cursor-pointer perspective"
+      onClick={() => setFlipped((prev) => !prev)}
+    >
+      <div
+        className={`relative w-full h-full transition-transform duration-500 preserve-3d ${
+          flipped ? "rotate-y-180" : ""
+        }`}
+      >
+        {/* Cara frontal */}
+        <div className="absolute inset-0 bg-white flashcard-bg rounded-xl shadow-lg p-6 flex flex-col justify-between hover:shadow-xl transition group backface-hidden">
 
-      {/* Contenido */}
-      <div className="p-2 border-gray-800 border-[1.5px] rounded-sm h-30 flex items-center justify-center">
-        <h3 className="text-xl font-semibold mx-2">{flashcard.question}</h3>
-      </div>
+          <div className="absolute top-3 right-3 flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+              className="p-2 rounded-full bg-blue-100 text-blue-600 hover:bg-blue-200 transition"
+              title="Editar"
+            >
+              <MdOutlineEdit />
+            </button>
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+              className="p-2 rounded-full bg-red-100 text-red-600 hover:bg-red-200 transition"
+              title="Eliminar"
+            >
+              <AiOutlineDelete />
+            </button>
+          </div>
 
-      <div className="mt-4 flex items-center gap-3 justify-between">
-        <span
-          className={`text-sm font-medium ${
-            flashcard.status === Status.LEARNED
-              ? "text-green-600"
-              : flashcard.status === Status.TO_REVIEW
-              ? "text-yellow-600"
-              : "text-gray-500"
-          }`}
-        >
-          {flashcard.status}
-        </span>
-        <div className="flex flex-wrap gap-2">
-          {flashcard.topics.map((t) => {
-            const color = topicColors[t] || {
-              bg: "bg-gray-200",
-              text: "text-gray-700",
-            };
-            return (
-              <span
-                key={t}
-                className={`px-2 py-1 rounded-full text-sm ${color.bg} ${color.text}`}
-              >
-                {t}
-              </span>
-            );
-          })}
+          <div className="p-2 border-gray-800 border-[1.5px] rounded-sm h-30 flex items-center justify-center">
+            <h3 className="text-xl font-semibold mx-2">{flashcard.question}</h3>
+          </div>
+
+          <div className="mt-4 flex items-center gap-3 justify-between">
+            <span
+              className={`text-sm font-medium ${
+                flashcard.status === Status.LEARNED
+                  ? "text-green-600"
+                  : flashcard.status === Status.TO_REVIEW
+                  ? "text-yellow-600"
+                  : "text-gray-500"
+              }`}
+            >
+              {flashcard.status}
+            </span>
+            <div className="flex flex-wrap gap-2">
+              {flashcard.topics.map((t) => {
+                const color = topicColors[t] || {
+                  bg: "bg-gray-200",
+                  text: "text-gray-700",
+                };
+                return (
+                  <span
+                    key={t}
+                    className={`px-2 py-1 rounded-full text-sm ${color.bg} ${color.text}`}
+                  >
+                    {t}
+                  </span>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+
+        {/* Cara trasera */}
+        <div className="absolute inset-0 bg-amber-50 rounded-xl shadow-lg p-6 flex items-center justify-center backface-hidden rotate-y-180">
+          <p className="text-lg text-gray-800">{flashcard.answer}</p>
         </div>
       </div>
     </article>
